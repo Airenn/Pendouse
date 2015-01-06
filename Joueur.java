960efb5a4df260f2cef39dd582846eab391bc7c5
Seeb77 <SebV@MacBook-Pro-de-Sebastien.local>
2015-01-06 08:57:31 +0100

    Retrait des @Objid

--- Joueur.java
+++ Joueur.java
@@ -1,44 +1,23 @@
-import com.modeliosoft.modelio.javadesigner.annotations.mdl;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("afc6d5dc-d407-4956-a969-fc9446dc146b")
 public class Joueur {
-    @mdl.prop
-    @objid ("0dd3d5c0-688a-4a40-981a-ef55c4ddaed2")
     private int vie;
+    private Lettre lettre;
 
-    @mdl.propgetter
     public int getVie() {
-        // Automatically generated method. Please do not modify this code.
         return this.vie;
     }
 
-    @mdl.propsetter
     public void setVie(int value) {
-        // Automatically generated method. Please do not modify this code.
         this.vie = value;
     }
 
-    @mdl.prop
-    @objid ("d789900f-6fcf-4ec6-b844-44ff97d4b4b5")
-    private Lettre lettre;
-
-    @mdl.propgetter
     public Lettre getLettre() {
-        // Automatically generated method. Please do not modify this code.
         return this.lettre;
     }
 
-    @mdl.propsetter
     public void setLettre(Lettre value) {
-        // Automatically generated method. Please do not modify this code.
         this.lettre = value;
     }
 
-    @objid ("5f845256-95db-4f21-a18e-fc29efcc6029")
-    public Lettre lettre;
-
-    @objid ("eef7527b-df81-4d1a-94bf-5c03af2e1cea")
     public int pointVie() {
     }
 
--- Lettre.java
+++ Lettre.java
@@ -1,30 +1,17 @@
-import com.modeliosoft.modelio.javadesigner.annotations.mdl;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("12715cba-ba8a-453a-b1f6-a38285001764")
 public class Lettre {
-    @mdl.prop
-    @objid ("eff8eed5-10a0-4b92-b22e-5cecff2a04df")
     private char lettre;
 
-    @mdl.propgetter
     public char getLettre() {
-        // Automatically generated method. Please do not modify this code.
         return this.lettre;
     }
 
-    @mdl.propsetter
     public void setLettre(char value) {
-        // Automatically generated method. Please do not modify this code.
         this.lettre = value;
     }
 
-    @objid ("ee5a1d47-9df6-44c3-8e30-2b546112b790")
     public boolean verifierLettre() {
     }
 
-    @objid ("b3f8e7f3-01eb-432b-b5c8-a078a81c286b")
     public boolean verifierLettreDansMot(String mot) {
     }
-
 }
