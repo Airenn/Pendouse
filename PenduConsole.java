bae6c2539c5ff8942ad969904439a406245ccd36
CcCvToa <Jean.Eudes.llas@gmail.com>
2015-01-06 09:59:27 +0100

    modifs afficherMot

--- PenduConsole.java
+++ PenduConsole.java
@@ -3,8 +3,26 @@ public class PenduConsole
 	private Joueur j;
 	private Dictionnaire d;
 	
-	public void afficherMot()
+	public void afficherMot(String mot)
 	{
+		int trouve[] = new int[mot.length()];
 		
+		for(int i = 0; i < mot.length(); i++)
+		{
+			if(trouve[i] == 0)
+			{
+				if(i == mot.length()-1)
+					System.out.println("_");
+				else
+					System.out.print("_.");
+			}
+			else
+			{
+				if(i == mot.length()-1)
+					System.out.println(mot.charAt(i));
+				else
+					System.out.print(mot.charAt(i));
+			}
+		}
 	}
 }
