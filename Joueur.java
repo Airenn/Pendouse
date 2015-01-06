2929f893eac0f2915cef4ec868faf66ad373c783
Airenn <nassim.ahmed.mahmoud@gmail.com>
2015-01-06 09:23:22 +0100

    Classe Joueur
    Complétion de la classe Joueur.

--- Joueur.java
+++ Joueur.java
@@ -1,7 +1,14 @@
 public class Joueur {
+
     private int vie;
     private Lettre lettre;
 
+    public Joueur(int vie, Lettre lettre) {
+		super();
+		this.vie = vie;
+		this.lettre = lettre;
+	}
+    
     public int getVie() {
         return this.vie;
     }
@@ -9,16 +16,22 @@ public class Joueur {
     public void setVie(int value) {
         this.vie = value;
     }
+    
+	public Lettre getLettre() {
+		return lettre;
+	}
 
-    public Lettre getLettre() {
-        return this.lettre;
+	public void setLettre(Lettre lettre) {
+		this.lettre = lettre;
+	}
+
+	public String toString() {
+		return "Joueur [vie=" + vie + ", lettre=" + lettre + "]";
+	}
+
+	public int pointVie(String mot) {
+		if(!this.lettre.verifierLettreDansMot(mot))
+			this.setVie(this.vie-1);
+		return this.vie;
     }
-
-    public void setLettre(Lettre value) {
-        this.lettre = value;
-    }
-
-    public int pointVie() {
-    }
-
 }
