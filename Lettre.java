af091dbeac7e81627a56f022cfa6a272380cc8ef
Airenn <nassim.ahmed.mahmoud@gmail.com>
2015-01-06 09:20:20 +0100

    Classe Lettre
    Complétion de la classe Lettre.

--- Lettre.java
+++ Lettre.java
@@ -1,17 +1,29 @@
 public class Lettre {
+
     private char lettre;
 
-    public char getLettre() {
-        return this.lettre;
+	public Lettre(char lettre) {
+		super();
+		this.lettre = lettre;
+	}
+
+	public char getLettre() {
+		return lettre;
+	}
+
+	public void setLettre(char lettre) {
+		this.lettre = lettre;
+	}
+
+    @Override
+	public String toString() {
+		return "Lettre [lettre=" + lettre + "]";
+	}
+
+	public boolean verifierLettreDansMot(String mot){
+    	if(mot.indexOf(this.lettre)!=-1)
+    		return true;
+    	return false;
     }
 
-    public void setLettre(char value) {
-        this.lettre = value;
-    }
-
-    public boolean verifierLettre() {
-    }
-
-    public boolean verifierLettreDansMot(String mot) {
-    }
 }
